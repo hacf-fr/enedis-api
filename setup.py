2a465cc5e8c71291451b41a4d791567870454cda
Quentin POLLET <polletquentin74@me.com>
2020-07-22 01:09:39 +0200

    Add setup download_url

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
+REPO_URL = "https://github.com/hacf-fr/enedis-api"
+VERSION = "0.0.1"
 
 if sys.version_info < (3, 6):
     sys.exit("Sorry, Python < 3.6 is not supported")
@@ -17,12 +17,13 @@ with open("README.rst", encoding="utf-8") as f:
 
 setup(
     name="enedis-api",
-    version="0.0.1",
+    version=VERSION,
+    url=REPO_URL,
+    download_url=REPO_URL + "/tarball/" + VERSION,
     description="Get your consumption data from your Enedis account (www.enedis.fr)",
     long_description=long_description,
     author="HACF-FR (Quentin POLLET)",
     author_email="polletquentin74@me.com",
-    url="https://github.com/hacf-fr/enedis-api",
     package_data={"": ["LICENSE"]},
     include_package_data=True,
     packages=find_packages(),
