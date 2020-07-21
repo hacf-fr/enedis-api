a3672d50f1431cd67bc45db7753e484ec84fdfd7
Quentin POLLET <polletquentin74@me.com>
2020-07-22 00:41:59 +0200

    Minimum version is 3.6

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 
-if sys.version_info < (3, 4):
-    sys.exit('Sorry, Python < 3.4 is not supported')
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -32,7 +32,6 @@ setup(
       license='Apache 2.0',
       install_requires=['python-dateutil', 'requests', 'simplejson', 'requests_oauthlib', 'oauthlib'],
       classifiers=[
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
